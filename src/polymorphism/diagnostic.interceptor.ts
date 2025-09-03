91d4bcfc77259cc56583046edb626d1b5d68f583
Zack Martin <zmartin@outsidesource.com>
2025-09-03 17:55:34 -0400

    Added an interceptor to add a custom response header

---
+++ src/polymorphism/diagnostic.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { GeneticRandomizerService } from './geneticRandomizer.service';
+
+@Injectable()
+export class DiagnosticInterceptor implements NestInterceptor {
+  constructor(private readonly geneticRandomizer: GeneticRandomizerService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        const response = context.switchToHttp().getResponse();
+        response.header('X-FakeSeed', this.geneticRandomizer.fakeSeed);
+        return data;
+      }),
+    );
+  }
+}
