3a67f7e346a01cd8a90fec64a71e5af286237601
Zack Martin <zmartin@outsidesource.com>
2025-10-28 17:43:08 -0400

    Added XML parser

---
+++ src/common/contentTypeParsers/xml.contentTypeParser.ts
@@ -0,0 +1,18 @@
+import { FastifyBodyParser, FastifyInstance, FastifyRequest } from 'fastify'
+import { CatParams, DogParams } from './types'
+import { XmlPetParserService } from './xmlPetParser.service'
+
+const xmlPetParserService: XmlPetParserService = new XmlPetParserService()
+
+const xmlContentTypeParser: FastifyBodyParser<string | Buffer> = async (
+  _request: FastifyRequest, 
+  rawBody: string | Buffer,
+): Promise<CatParams | DogParams | undefined> => {
+  return xmlPetParserService.parsePet(rawBody.toString())
+}
+
+export const XmlContentTypeParserParams: Parameters<FastifyInstance['addContentTypeParser']> = [
+  ['application/xml', 'text/xml'],
+  { parseAs: 'string' },
+  xmlContentTypeParser,
+]
